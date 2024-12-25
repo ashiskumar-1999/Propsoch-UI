01b6a6aedce1a38052612de7a6c29ee3ec613054
Ashis Kumar <ashishsahoo389@gmail.com>
2024-12-25 17:20:09 +0530

    Create wishlists.js

---
+++ src/pages/wishlists.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '@/styles/Home.module.css'
+
+const wishlists = () => {
+  return (
+    <div className={styles.heading}>Add any property as your favourite to see the wishlist.</div>
+  )
+}
+
+export default wishlists
