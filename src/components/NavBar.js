2dc959b5c1d7e6951278dc8bce87c0dc3d31068a
Ashis Kumar <ashishsahoo389@gmail.com>
2024-12-25 17:19:28 +0530

    Fix: Navitem icon css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import Link from 'next/link';
 import styles from '@/styles/Home.module.css'
+import wishlists from '@/pages/wishlists';
 import { CiSearch, CiLocationOn, CiUser } from "react-icons/ci";
 import { IoIosHeartEmpty } from "react-icons/io";
 
@@ -8,21 +10,29 @@ import { IoIosHeartEmpty } from "react-icons/io";
 const NavBar = () => {
   return (
     <div className={styles.navbar}>
-        <div className={styles.navitems}>
-            <CiSearch />
-            <p className={styles.navitem} onClick={styles.navitem.selected}>Explore</p>
+        <div className={styles.navitems && styles.navitems.selected}>
+            <Link href="/">
+            <CiSearch className={styles.navicon} />
+            <p className={styles.navitem} >Explore</p>
+            </Link>
         </div>
         <div className={styles.navitems}>
-            <IoIosHeartEmpty />
+            <Link href="/wishlists">
+            <IoIosHeartEmpty className={styles.navicon} />
             <p className={styles.navitem} onClick={styles.navitem.selected}>WishList</p>
+            </Link>
         </div>
         <div className={styles.navitems}>
-            <CiLocationOn />
+            <Link href="/">
+            <CiLocationOn className={styles.navicon} />
             <p className={styles.navitem} onClick={styles.navitem.selected}>Show Map</p>
+            </Link>
         </div>
         <div className={styles.navitems}>
-            <CiUser />
+        <Link href="/">
+            <CiUser className={styles.navicon} />
             <p className={styles.navitem} onClick={styles.navitem.selected}>Log in</p>
+            </Link>
         </div>
         
     </div>
